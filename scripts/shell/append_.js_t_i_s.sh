34c16c27adda5f3b37c5d7dbc237001e652823b3
Stanford47 <scoding@mailfence.com>
2023-04-01 00:39:28 -0700

    p

--- scripts/shell/append_.js_t_i_s.sh
+++ scripts/shell/append_.js_t_i_s.sh
@@ -1,1 +1,23 @@
-find ./dist/src >> ./scripts/tmp/DIST_JS_FILE_LIST.txt
+find ./dist/src -name "*.js" >> ./scripts/tmp/DIST_JS_FILE_LIST.txt
+
+# file_array=()
+
+# https://stackoverflow.com/a/73075563 comment #3
+#SEARCH_REGEX="/(from\s+)(["'])(?!.*\.js)(\.?\.\/.*)(["'])/"
+
+# get_file_contents() {
+#     while IFS= read -r line
+#     do 
+#         file_array+=("$line")
+#         echo 
+#     done < "$1"
+# }
+
+# get_file_contents "./scripts/tmp/DIST_JS_FILE_LIST.txt"
+
+# rm ./scripts/tmp/DIST_JS_FILE_LIST.txt
+
+# for e in "${file_array[@]}"
+# do 
+#     echo "$e"
+# done
